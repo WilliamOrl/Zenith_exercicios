1c0582fc96304f53b6fc8cb6dc02512f8ea7e82f
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-04-23 11:37:38 -0300

    Update Paint no Terminal.cpp

--- Paint no Terminal/Paint no Terminal.cpp
+++ Paint no Terminal/Paint no Terminal.cpp
@@ -1,72 +1,123 @@
 #include<stdio.h>
+#define SUCESSO 1
+#define ERRO -1
 
-int Comandos(char *retorno);		//Le o comando digitado
+int Comandos(int *retorno);		//Le o comando digitado
+void Create(int *dimensoes);		//Cria a imagem PBM
 
 int main(void){
-	char cmd[30];
+	int cmd[30];
 	int aux;
 	
-	aux = Comandos(cmd);
-	switch(aux){
-		
-		case 1:
+	while(1){
+		aux = Comandos(cmd);
+		switch(aux){
 			
-			break;
-		
-		
-		case 2:
+			case 1:
+				Create(cmd);
+				break;
 			
-			break;
-		
-		
-		case 3:
 			
-			break;
-		
-		
-		case 4:
+			case 2:
+				
+				break;
 			
-			break;
-		
-		
-		case 5:
 			
-			break;
-		
+			case 3:
+				
+				break;
+			
+			
+			case 4:
+				
+				break;
+			
+			
+			case 5:
+				
+				break;
+			
+			case 6:
+				
+				break;
+			
+		}	
 	}
-	
-	
-	
 }
 
 
-int Comandos(char *retorno){
+int Comandos(int *retorno){
 	
+	int i,j,aux;
 	char cmd[30];
-	scanf("%s",cmd);
+	
+	printf("Digite o Comando: ");
+	scanf("%s",&cmd);
+	//printf("\n");
+	
+	aux = SUCESSO;
 	
 	if(cmd[0] == 'C' && cmd[1] == 'R' && cmd[2] == 'E' && cmd[3] == 'A' && cmd[4] == 'T' && cmd[5] == 'E'){		//CREATE
-		return 0;
 		
+		for(i=6,j=0;i=30;i++){
+			if(cmd[i] != ' '){			//32 = espaço	
+				aux = cmd[i] - 48;		//não le o espaço entre os numeros
+				retorno[j] = aux;
+				j++;
+			}
+		}
 	}
+		return 1;
+		
 	
 	if(cmd[0] == 'E' && cmd[1] == 'X' && cmd[2] == 'P' && cmd[3] == 'O' && cmd[4] == 'R' && cmd[5] == 'T'){		//EXPORT
-		return 1;
-	}
+
+		for(i=4,j=0;i=30;i++,j++)
+		
+			cmd[i]= retorno[j];
+
 	
-	if(cmd[0] == 'L' && cmd[1] == 'I' && cmd[2] == 'N' && cmd[3] == 'E'){		//LINE
 		return 2;
 	}
 	
-	if(cmd[0] == 'C' && cmd[1] == 'I' && cmd[2] == 'R' && cmd[3] == 'C' && cmd[4] == 'L' && cmd[5] == 'E'){		//CIRCLE
+	if(cmd[0] == 'L' && cmd[1] == 'I' && cmd[2] == 'N' && cmd[3] == 'E'){		//LINE
+		
+		for(i=4,j=0;i=30;i++,j++)
+		
+			cmd[i]= retorno[j];
+			
 		return 3;
 	}
 	
-	if(cmd[0] == 'D' && cmd[1] == 'I' && cmd[2] == 'S' && cmd[3] == 'K'){		//DISK
+	if(cmd[0] == 'C' && cmd[1] == 'I' && cmd[2] == 'R' && cmd[3] == 'C' && cmd[4] == 'L' && cmd[5] == 'E'){		//CIRCLE
+		
+		for(i=6,j=0;i=30;i++,j++)
+			cmd[i]= retorno[j];
+		
 		return 4;
 	}
 	
-	if(cmd[0] == 'R' && cmd[1] == 'E' && cmd[2] == 'C' && cmd[3] == 'T'){		//RECT
+	if(cmd[0] == 'D' && cmd[1] == 'I' && cmd[2] == 'S' && cmd[3] == 'K'){		//DISK
+		
+		for(i=4,j=0;i=30;i++,j++)
+			cmd[i]= retorno[j];
+		
 		return 5;
 	}
+	
+	if(cmd[0] == 'R' && cmd[1] == 'E' && cmd[2] == 'C' && cmd[3] == 'T'){		//RECT
+		
+		for(i=4,j=0;i=30;i++,j++)
+			cmd[i]= retorno[j];
+		
+		return 6;
+	}
+}
+
+void Create(int *dimensoes){
+	int i;
+	
+	for(i=0;i!=6;i++)
+	printf("dim[%d] = %d\n",i,dimensoes[i]);
+	return;
 }
